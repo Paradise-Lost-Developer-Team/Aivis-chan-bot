39142d231bd6596e73e5271d36f2012f97aeabfe
BuildTools <unconfigured@null.spigotmc.org>
2025-02-20 15:40:12 +0900

    aa

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({ path: '.env', override: true });
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env', override: true });
 const TOKEN = process.env.TOKEN;
