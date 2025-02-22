cbf24d9b2ddc1867807c447786ab4f1ee1d76520
Alexander-0520 <uket.panda.1st@gmail.com>
2025-02-23 03:31:50 +0900

    gg

--- commands/utility/edit_word.ts
+++ commands/utility/edit_word.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, CommandInteractionOptionResolver } from 'discord.js';
+import { fetchAllUUIDs, updateGuildDictionary, wordTypeChoices } from 'dictionaries';
+import { CommandInteraction, CommandInteractionOptionResolver, MessageFlags } from 'discord.js';
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -20,7 +21,8 @@ module.exports = {
         .addStringOption(option =>
             option.setName('word_type')
                 .setDescription('単語の種類')
-                .setRequired(true)),
+                .setRequired(true)
+                .setChoices(wordTypeChoices)),
     async execute(interaction: CommandInteraction) {
         const options = interaction.options as CommandInteractionOptionResolver;
         const word = options.getString('word', true);
@@ -28,7 +30,20 @@ module.exports = {
         const accentType = options.getNumber('accent_type', true);
         const wordType = options.getString('word_type', true);
 
-        // 辞書の単語を編集する処理をここに記述
-        await interaction.reply(`単語 '${word}' を編集しました。`);
+
+        const uuidDict = await fetchAllUUIDs();
+        const uuid = Object.keys(uuidDict).find(key => uuidDict[key] === word);
+        const editUrl = `http://localhost:10101/user_dict_word/${uuid}?surface=${word}&pronunciation=${newPronunciation}&accent_type=${accentType}&word_type=${wordType}`;
+        if (uuid) {
+            const response = await fetch(editUrl, { method: 'PUT' });
+            // 辞書の単語を編集する処理をここに記述
+            if (response.status === 204) {
+                const details = { pronunciation: newPronunciation, accentType, wordType };
+                updateGuildDictionary(interaction.guildId!, word, details);
+                await interaction.reply(`単語 '${word}' の発音を '${newPronunciation}', アクセント '${accentType}', 品詞 '${wordType}' に編集しました。`);
+            } else {
+                await interaction.reply({ content: '単語の編集に失敗しました。', flags: MessageFlags.Ephemeral });
+            }
+        }
     },
 };
