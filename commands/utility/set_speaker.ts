24684029fa65d340f072ca3a75a6bc5ef30ddbe1
Alexander-0520 <uket.panda.1st@gmail.com>
2025-02-23 05:34:46 +0900

    gg

--- commands/utility/set_speaker.ts
+++ commands/utility/set_speaker.ts
@@ -1,30 +1,40 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, CommandInteractionOptionResolver, MessageFlags } from 'discord.js';
-import { getSpeakerChoices, speakers, currentSpeaker, loadSpeakers } from '../../TTS-Engine';
+import { ActionRowBuilder, CommandInteraction, StringSelectMenuBuilder, Interaction, Events } from 'discord.js';
+import { speakers } from '../../TTS-Engine';
+import { client } from '../../index';
+import { currentSpeaker } from '../../TTS-Engine';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("set_speaker")
-        .setDescription("スピーカーを設定する")
-        .addNumberOption(option => 
-        option.setName("speaker")
-            .setDescription("設定するスピーカーのID")
-            .setRequired(true)
-        ),
+        .setDescription("スピーカーを設定する"),
     async execute(interaction: CommandInteraction) {
-        loadSpeakers();
         if (speakers.length === 0) {
             await interaction.reply("スピーカー情報が読み込まれていません。");
             return;
         }
-
-        const options = interaction.options as CommandInteractionOptionResolver;
-        const speaker = options.getNumber('speaker', true);
-        if (speaker !== null) {
-            currentSpeaker[interaction.guildId!] = speaker;
-            await interaction.reply(`話者を ${speakers[speaker]} に設定しました。`);
-        } else {
-        await interaction.reply({ content: '無効な話者です。', flags: MessageFlags.Ephemeral });
+        const options = speakers.map(speaker => ({
+            label: speaker.name,
+            value: speaker.id.toString()
+        }));
+        const row = new ActionRowBuilder<StringSelectMenuBuilder>()
+            .addComponents(
+                new StringSelectMenuBuilder()
+                    .setCustomId('select_speaker')
+                    .setPlaceholder('話者を選択してください')
+                    .addOptions(options)
+            );
+        await interaction.reply({ content: "話者を選択してください:", components: [row] });
     }
-},
 };
+
+client.on(Events.InteractionCreate, async (interaction: Interaction) => {
+    if (!interaction.isStringSelectMenu()) 
+        return;
+    
+    if (interaction.customId === 'select_speaker') {
+        const selectedSpeakerId = interaction.values[0];
+        currentSpeaker[interaction.guildId!] = parseInt(selectedSpeakerId);
+        await interaction.update({ content: `話者をID ${selectedSpeakerId} に設定しました。`, components: [] });
+    }
+})
