557e815d246b2fade04be26bdf0e86d793853aa1
AlexanderGG-0520 <uket.panda.1st@gmail.com>
2025-04-22 20:55:50 +0900

    fix: 不要なインポートを削除

--- utils/subscription.ts
+++ utils/subscription.ts
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { Client, Guild } from 'discord.js';
 import { client } from '../index';
 import { logError } from './errorLogger';
 
