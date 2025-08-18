75c4f3a195cacdb84fc3ff74bf9f0f99fb91ad7e
AlexanderGG-0520 <uket.panda.1st@gmail.com>
2025-08-18 14:28:40 +0900

    feat: ステータスAPIサーバーのポートを3002から3006に変更

--- index.ts
+++ index.ts
@@ -276,8 +276,8 @@ apiApp.get('/api/stats', (req: Request, res: Response) => {
 
     res.json({ serverCount, vcCount, userCount, uptimeRate });
 });
-apiApp.listen(3002, () => {
-    console.log('Stats APIサーバーがポート3002で起動しました');
+apiApp.listen(3006, () => {
+    console.log('Stats APIサーバーがポート3006で起動しました');
 });
 // --- ここまで追加 ---
 
