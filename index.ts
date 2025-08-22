b07c5bc3620bb2f4c26f8f1f6d2a3f93dd8fb8e3
AlexanderGG-0520 <uket.panda.1st@gmail.com>
2025-08-23 01:27:06 +0900

    feat: AIチャット機能を強化し、会話履歴の管理と音声設定の保存機能を追加

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { deployCommands } from "./utils/deploy-commands";
 import { REST } from "@discordjs/rest";
 import * as fs from "fs";
 import * as path from "path";
-import { AivisAdapter, loadAutoJoinChannels, loadJoinChannels, loadSpeakers, fetchAndSaveSpeakers, loadUserSpeakers } from "./utils/TTS-Engine";
+import { AivisAdapter, loadAutoJoinChannels, loadJoinChannels, loadSpeakers, fetchAndSaveSpeakers, loadUserVoiceSettings } from "./utils/TTS-Engine";
 import { ServerStatus, fetchUUIDsPeriodically } from "./utils/dictionaries";
 import { MessageCreate } from "./utils/MessageCreate";
 import { VoiceStateUpdate } from "./utils/VoiceStateUpdate";
@@ -116,7 +116,7 @@ client.once("ready", async () => {
         loadAutoJoinChannels();
         loadJoinChannels();
         loadSpeakers();
-        loadUserSpeakers();
+        loadUserVoiceSettings();
         
         console.log("TTS初期化完了");
 
@@ -276,8 +276,8 @@ apiApp.get('/api/stats', (req: Request, res: Response) => {
 
     res.json({ serverCount, vcCount, userCount, uptimeRate });
 });
-apiApp.listen(3005, () => {
-    console.log('Stats APIサーバーがポート3005で起動しました');
+apiApp.listen(3002, () => {
+    console.log('Stats APIサーバーがポート3002で起動しました');
 });
 // --- ここまで追加 ---
 
